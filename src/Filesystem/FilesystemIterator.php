6c3b2f3ad09bd31d23dcb21c028c9547dcea4a83
Mike Fisher <mike@mbfisher.com>
2014-04-23 09:08:53 +0100

    First commit; branch from main repo

---
+++ src/Filesystem/FilesystemIterator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Task\Plugin\Filesystem;
+
+use Task\Plugin\Stream\ReadableInterface;
+use Task\Plugin\Stream\WritableInterface;
+
+class FilesystemIterator extends \RecursiveIteratorIterator implements ReadableInterface
+{
+    public function __construct(
+        $path,
+        $directoryOptions = \FilesystemIterator::SKIP_DOTS,
+        $iteratorOptions = null
+    ) {
+        parent::__construct(
+            new \RecursiveDirectoryIterator(
+                $path,
+                $directoryOptions
+            ),
+            $iteratorOptions
+        );
+    }
+
+    public function read()
+    {
+        return $this;
+    }
+
+    public function pipe(WritableInterface $to)
+    {
+        return $to->write($this->read());
+    }
+}
